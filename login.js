ddcd33055d208cc618ab05ac8ba839c88430376b
amir <118857034+ABHVBJGCNVSTU@users.noreply.github.com>
2024-11-20 12:11:28 +0500

    Delete login.js

--- login.js
+++
@@ -1,9 +0,0 @@
-const { login } = require('fb-chat-api');
-const config = require('./config.json');
-
-login({ email: config.email, password: config.password }, (err, api) => {
-  if (err) return console.error("Login failed:", err);
-
-  console.log('Logged in successfully!');
-  require('./index.js')(api);
-});
